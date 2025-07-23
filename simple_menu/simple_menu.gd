84465d135c9577c5aa76c4efba1aa8f83299ded0
Michal Sevcik <michal.sevcik@protonmail.com>
2025-07-23 17:33:16 +0200

    Add menu and chicken scene basic

---
+++ simple_menu/simple_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func load_game_scene(path: String) -> void:
+	var scene = load(path)
+	get_tree().change_scene_to_packed(scene)
+
+
+func _on_cow_pressed() -> void:
+	load_game_scene("res://cow_milking/cow_milking.tscn")
+
+
+func _on_chicken_pressed() -> void:
+	load_game_scene("res://chicken_eggs/chicken_eggs.tscn")
