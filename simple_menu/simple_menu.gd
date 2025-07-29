979693985545d3dd04db152cf4033f0f1ab5e421
Michal Sevcik <michal.sevcik@protonmail.com>
2025-07-29 18:31:13 +0200

    Fix load scene to call after frame

--- simple_menu/simple_menu.gd
+++ simple_menu/simple_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func load_game_scene(path: String) -> void:
 	var scene = load(path)
-	get_tree().change_scene_to_packed(scene)
+	get_tree().change_scene_to_packed.call_deferred(scene)
 
 
 func _on_cow_pressed() -> void:
