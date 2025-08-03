3d7474bfed61448e61a61b8648e542287ea08fe8
Michal Sevcik <michal.sevcik@protonmail.com>
2025-08-03 21:22:09 +0200

    Add celebratory animation on level up

--- global/experience/experience.gd
+++ global/experience/experience.gd
@@ -6,9 +6,15 @@ var _level_label: Label = get_node("CenterContainer/VBoxContainer/Label")
 @onready
 var _level_progress: ProgressBar = get_node("CenterContainer/VBoxContainer/ProgressBar")
 
+@onready
+var _level_celebration: CPUParticles2D = get_node("CPUParticles2D")
+
 func update_level(level: int) -> void:
 	_level_label.text = "LVL: " + str(level)
+	_celebrate()
 	
 func update_progress(value: float) -> void:
-	print(value)
 	_level_progress.value = value
+
+func _celebrate() -> void:
+	_level_celebration.emitting = true
