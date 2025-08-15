150bfb1eac51f7a67fc13654c64e042e5a14b9fe
Michal Sevcik <michal.sevcik@protonmail.com>
2025-08-15 20:13:41 +0200

    Add sheers sound

--- shears/shears.gd
+++ shears/shears.gd
@@ -3,14 +3,19 @@ extends Node3D
 @export
 var camera: Camera3D
 
+@onready
+var _buzz_sound: AudioStreamPlayer3D = get_node("BuzzSound")
+
 var ray_length: float = 1000.0
 var debug_ray: ImmediateMesh
 
 func _input(event) -> void:
-	if event is InputEventScreenDrag:
+	if event is InputEventScreenDrag or event is InputEventMouseMotion:
 		_move_to_finger(event.position)
 
 func _move_to_finger(screen_pos: Vector2) -> void:
+	if not _buzz_sound.playing:
+		_buzz_sound.play()
 	Globals.vibrate(10, 0.1)
 	visible = false
 	var from = camera.project_ray_origin(screen_pos)
