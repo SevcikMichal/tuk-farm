df6dba40c63ed5ad441215c0bc8eb24cde6036b9
Michal Sevcik <michal.sevcik@protonmail.com>
2025-08-19 18:36:52 +0200

    Update hints

--- global/hint/hint.gd
+++ global/hint/hint.gd
@@ -3,12 +3,18 @@ extends Control
 @export
 var hint_text: String
 
+@export
+var hint_panel_y_size: int = 500
+
 var _hint_label: Label
+var _hint_panel: Panel
 
 func _ready() -> void:
-	_hint_label = get_node("CenterContainer/Label")
+	_hint_panel = get_node("CenterContainer/HintPanel")
+	_hint_label = get_node("CenterContainer/HintPanel/Label")
+	_hint_panel.custom_minimum_size.y = hint_panel_y_size
 	_hint_label.text = hint_text
-	_hint_label.visible = Globals.get_configuration().show_hints()
+	_hint_panel.visible = Globals.get_configuration().show_hints()
 
 func _on_timer_timeout() -> void:
-	_hint_label.visible = false
+	_hint_panel.visible = false
